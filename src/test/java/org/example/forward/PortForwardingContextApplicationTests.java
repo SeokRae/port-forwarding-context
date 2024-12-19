3a5954ef427a087dcdfda3ced8dad8e782d15e92
SeokRae <kslbsh@gmail.com>
2024-12-19 13:29:16 +0900

    Initial commit

---
+++ src/test/java/org/example/forward/PortForwardingContextApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.forward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortForwardingContextApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
